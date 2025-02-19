160572a605dce428a591321465ff0bfa96808d46
Roman Lefler <rlefler001@csbsju.edu>
2025-02-19 09:18:51 -0600

    Add regression test for save school not working with multiple schools

---
+++ test/regression/SaveSchoolThrowsUnexpectedly.java
@@ -0,0 +1,44 @@
+package regression;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cmc.CMCException;
+import cmc.backend.DatabaseController;
+import junit.framework.Assert;
+
+public class SaveSchoolThrowsUnexpectedly {
+
+	private static final String USERNAME = "test_test_test_ayo";
+	private static final String PASSWD = "onskibidi73";
+	
+	private DatabaseController db;
+	
+	@Before
+	public void setUp() throws CMCException {
+		db = new DatabaseController();
+		db.addUser(USERNAME, PASSWD, 'u', "lebron", "james");
+		db.deactivateUser(USERNAME);
+	}
+	
+	@After
+	public void tearDown() throws CMCException {
+		db.removeUser(USERNAME);
+	}
+	
+	@Test
+	public void test() {
+		boolean succ = db.saveSchool(USERNAME, "BARD");
+		Assert.assertTrue(succ);
+		
+		succ = db.saveSchool(USERNAME, "BROWN");
+		Assert.assertTrue(succ);
+		
+		succ = db.saveSchool(USERNAME, "BAYLOR");
+		Assert.assertTrue(succ);
+	}
+
+}
